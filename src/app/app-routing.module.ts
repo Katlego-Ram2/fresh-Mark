0117f771fe86aac68f7853299d2ac1d48e0256e2
katlego.rampedi <katlego.rampedi@Ekurhuleni.gov.za>
2023-08-02 14:33:46 +0200

    added login design -katlego

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
-import { NgModule } from '@angular/core';
+import { NgModule, createComponent } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { CreateAccountComponent } from './create-account/create-account.component';
+import { LoginComponent } from './login/login.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:"",pathMatch:"full",component:AppComponent
+  },
+  {
+    path:"create",component:CreateAccountComponent
+  },
+  {
+    path:"login",component:LoginComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
